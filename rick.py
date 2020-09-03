8acc26bbc068e23f574e52d17623e7bb5d0c50c2
ONXLive <talk@onyxcode.space>
2020-09-03 16:59:56 -0400

    Initial commit

---
+++ rick.py
@@ -0,0 +1,135 @@
+import webbrowser
+import random
+
+
+def rick():
+    if random.randrange(1, 6) == 1:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+
+    if random.randrange(1, 6) == 2:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+    if random.randrange(1, 6) == 3:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+    if random.randrange(1, 6) == 4:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+    if random.randrange(1, 6) == 5:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+    if random.randrange(1, 6) == 6:
+        choice = input("Pick a number between one and six.\n")
+
+        if choice == "1":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "2":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "3":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "4":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "5":
+            webbrowser.open_new("https://onyxcode.github.io/rickroulette/safe")
+            exit(0)
+        elif choice == "6":
+            webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            exit(0)
+
+rick()
+exit(0)
